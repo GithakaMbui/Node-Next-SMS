6e00e8e9600db0a17896c929b1b42ec1a7c07bab
Githaka <githakamaru@gmail.com>
2020-09-24 21:16:36 +0300

    implemented api get and post requests

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = reqire('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const Nexmo = require('nexmo');
@@ -6,3 +6,35 @@ const socketio = require('socket.io');
 
 //Init app
 const app = express();
+
+//Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+
+//Template folder setup
+app.use(express.static(__dirname + '/public'));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Index Route
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+//Catch form submit
+app.post('/', (req, res) => {
+    res.send(req.body);
+    console.log(req.body);
+});
+
+//Define Port
+const port = 4000;
+
+//Start server
+const server = app.listen(port, () => console.log(`Server started on 
+port ${port}`));
