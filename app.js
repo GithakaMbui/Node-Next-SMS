d3a574982dc11be7f993671e4ce4770b33e2244d
Githaka <githakamaru@gmail.com>
2020-09-24 20:00:29 +0300

    installed dependencies,initialized express, package.json, setup middleware and initialized a public folder

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = reqire('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+//Init app
+const app = express();
